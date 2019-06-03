dbee93b086ef3e9d3ee5a6d1f40a029b5b668b28
significance <dan@1up.digital>
2019-06-03 14:03:53 +0100

    extra bits for swarmhole.com

--- src/App.js
+++ src/App.js
@@ -10,11 +10,57 @@ class App extends Component {
   constructor(props) {
     super(props);
 
+    var urlParams = new URLSearchParams(window.location.search);
+    let recipient = urlParams.get('recipient');
+
     this.state = {
+      recipient: recipient,
       isSending: false,
-      hasSent: false
+      hasSent: false,
+      feedback: '',
+      uploaded: 0,
+      found: false
     };
 
+  }
+
+  setFeedback(feedback){
+    this.setState({
+      feedback: feedback
+    });
+  }
+
+  setUploaded(uploaded){
+    this.setState({
+      uploaded: uploaded
+    });
+  }  
+
+  async setAccount(event){
+    event.preventDefault();
+    let account = event.target.value;
+    let fds = new FDS();
+    this.setState({
+      found: false,
+      finding: true,
+      feedback: 'finding account...',
+    })        
+    let availiable = await fds.Account.isMailboxNameAvailable(account);
+    if(availiable === false){
+      this.setState({
+        found: true,
+        finding: false,
+        feedback: 'found! your Swarmhole is at...',
+        swarmholeAddress: "https://swarmhole.com/?recipient="+account
+      })      
+    }else{
+      this.setState({
+        found: false,
+        finding: false,
+        feedback: 'couldn\'t find that account, please register at fairdrop.xyz',
+        swarmholeAddress: "https://swarmhole.com/?recipient="+account
+      })        
+    }
   }
 
   componentDidMount(){
@@ -23,18 +69,38 @@ class App extends Component {
     this.dropzone = new Dropzone(this.refs.dz, {
       url: 'dummy://', //dropzone requires a url even if we're not using it
       previewsContainer: false,
+      clickable: true,
       maxFilesize: 1000,
       accept: async (file, done) => {
 
         this.setState({
-          isSending: true
+          isSending: true,
+          status: 'Creating Account'
         });
 
-        //send file to server     
+        //create account
 
         let random = Math.floor(Math.random() * 101010101);
-        let account = await fds.CreateAccount(`${random}-swarmhole-throwaway`,'');
-        await account.send('swarm19', file);           
+        let account = await fds.CreateAccount(
+          `${random}-swarmhole-throwaway`,
+          '',
+          this.setFeedback.bind(this)
+        );
+
+        this.setState({
+          isSending: true,
+          status: 'Sending'
+        });
+
+        //send file to server
+
+        await account.send(
+          'swarm19', 
+          file, 
+          this.setFeedback.bind(this), 
+          this.setUploaded.bind(this),
+          this.setFeedback.bind(this)
+        );
 
         this.setState({
           hasSent: true
@@ -53,24 +119,53 @@ class App extends Component {
           <div className="Swarmhole-logo"><a href="/"><img src="/swarmhole.svg" alt="Swarmhole-logo"/></a></div>
           <div className="Swarmhole-widget-wrapper col-lg-6 col-sm-8">
               <div className="Swarmhole-widget-inner">
-                { this.state.isSending === false && this.state.isSending === false &&
+                { this.state.recipient !== null && 
                   <div>
-                    <p>DROP FILE HERE TO SEND TO</p> 
-                    <p>swarm19.datafund.eth</p>
+                      { this.state.isSending === false && this.state.isSending === false &&
+                        <div>
+                          <p>DROP A FILE HERE TO SEND TO...</p> 
+                          <p>{this.state.recipient}.datafund.eth</p>
+                        </div>
+                      }
+                      { (this.state.isSending !== false && this.state.hasSent === false) &&
+                        <div>
+                          <p>{this.state.status} <img className="Swarmhole-spinning-star" src="/fds-star-small.svg"/></p> 
+                          <p className="feedback">{this.state.feedback}</p>
+                          {this.state.uploaded > 0 &&
+                            <p className="uploaded">{this.state.uploaded}%</p>
+                          }
+                        </div>
+                      }
+                      { this.state.hasSent !== false &&
+                        <div>
+                          <p>Sent!</p>
+                        </div>
+                      }
                   </div>
                 }
-                { (this.state.isSending !== false && this.state.hasSent === false) &&
+                { this.state.recipient === null && 
                   <div>
-                    <p>Sending <img className="Swarmhole-spinning-star" src="/fds-star-small.svg"/></p> 
-                  </div>
-                }
-                { this.state.hasSent !== false &&
-                 <div>
-                    <p>Sent!</p>
+                    <div>
+                      <p>Enter your <a target="_blank" href="https://fairdrop.xyz">fairdrop.xyz</a><br/>account name...</p> 
+                      <input type="text" onChange={this.setAccount.bind(this)}></input>
+                        <p>
+                          {this.state.feedback}
+                          <br/>
+                          { this.state.found &&
+                            <a target="_blank" href={this.state.swarmholeAddress}>{this.state.swarmholeAddress}</a>
+                          }
+                        </p>
+                    </div>
                   </div>
                 }
               </div>
           </div> { /* Swarmhole-widget-wrapper */ }
+        </div>
+        <div className="Powered-by">
+          powered by... <br/>
+          <a target="_blank" href="https://github.com/fairDataSociety/fds.js">
+            fds.js
+          </a>
         </div>
       </div>
     );
